f2635e7ded888b6983487ba8d342004535940a6f
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-05-15 21:21:10 -0600

    feat: add test cases for login

--- __tests__/pages/login.test.tsx
+++ __tests__/pages/login.test.tsx
@@ -10,14 +10,61 @@ const Wrapper: React.FunctionComponent<React.PropsWithChildren> = ({
     children,
 }) => <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>;
 
-beforeEach(() => {
-    render(<Login />, { wrapper: Wrapper });
-});
-
-it('should render login page properly', async () => {
-    const heading = screen.getByRole('heading', {
-        name: /log in to spotify/i,
+describe('Login page', () => {
+    beforeEach(() => {
+        render(<Login />, { wrapper: Wrapper });
     });
 
-    expect(heading).toBeInTheDocument();
+    it('should render properly', () => {
+        expect(
+            screen.getByRole('heading', {
+                name: /log in to spotify/i,
+            }),
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByRole('textbox', { name: /email/i }),
+        ).toBeInTheDocument();
+
+        expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+
+        expect(
+            screen.getByRole('button', { name: /log in/i }),
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByRole('link', {
+                name: /sign up for spotify/i,
+            }),
+        ).toBeInTheDocument();
+    });
+
+    it('should show error when inputs are empty', async () => {
+        const loginButton = screen.getByRole('button', { name: /log in/i });
+
+        userEvent.click(loginButton);
+
+        expect(
+            await screen.findByText(
+                /Please enter your username or email address./i,
+            ),
+        ).toBeVisible();
+
+        expect(
+            await screen.findByText(/Please enter your password./i),
+        ).toBeVisible();
+    });
+
+    it('should show error when user is invalid', async () => {
+        const emailInput = screen.getByRole('textbox', { name: /email/i });
+        const passwordInput = screen.getByLabelText(/password/i);
+        const loginButton = screen.getByRole('button', { name: /log in/i });
+
+        await userEvent.type(emailInput, 'johny');
+        await userEvent.type(passwordInput, '123456');
+
+        userEvent.click(loginButton);
+
+        screen.debug();
+    });
 });
