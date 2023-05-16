f8d79594c1fbcb4c5156b2f314dd329246c99ca0
Alexander Apaza <alrus2797@gmail.com>
2023-05-15 20:42:26 -0500

    feat: add home view

---
+++ src/main-view/home-view/home-view.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HomeDiv = styled.div`
+  height: 1000px;
+`;
+
+function HomeView() {
+  return <HomeDiv>Home</HomeDiv>;
+}
+
+export { HomeView };
--- src/main-view/main-view.tsx
+++ src/main-view/main-view.tsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
+import { HomeView } from './home-view/home-view';
 
 const MainDiv = styled.div`
-  grid-area: 'main-view';
+  grid-area: main-view;
   background-color: #121212;
+  overflow-y: scroll;
 `;
 
 function MainView() {
   return (
     <MainDiv>
       <Routes>
-        <Route index element={<div>Main</div>} />
+        <Route index element={<HomeView />} />
         <Route path="search" element={<div>Search</div>} />
         <Route path="login" element={<div>Login</div>} />
       </Routes>
