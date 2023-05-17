3379fefc1f453fdc4db40311c8acc4340f569fdd
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-05-17 08:31:00 -0600

    refactor: follow naming conventions

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { SignUpContainer } from '../../components/Styles/SignUp/SignUpContainer.styles';
+import { StyledSignUpContainer } from '../../components/Styles/SignUp/SignUpContainer.styles';
 import { Navigate } from 'react-router';
 import { StyledLogo } from '../../components/Styles/SignUp/Logo.styles';
 
 const SignUp: React.FunctionComponent = () => {
     return (
-        <SignUpContainer>
+        <StyledSignUpContainer>
             <StyledLogo href="/">
                 <img src="/svg/logo-extended.svg" alt="" />
             </StyledLogo>
             <h2>Sign up for free to start listening.</h2>
-        </SignUpContainer>
+        </StyledSignUpContainer>
     );
 };
 
