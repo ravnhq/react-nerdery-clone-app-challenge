738e3b7b54958ab07a097b83bff920fcbc2906db
Alexander Apaza <alrus2797@gmail.com>
2023-05-18 18:16:55 -0500

    feat: add font color globalStyle

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -14,6 +14,7 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     height: 100%;
     line-height: 1.6;
+    color: #b3b3b3;
   }
 
   a, abbr, acronym, address, applet, article, aside, audio, b, big, blockquote, body, canvas, caption, center, cite, code, dd, del, details, dfn, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, header, hgroup, html, i, iframe, img, ins, kbd, label, legend, li, mark, menu, nav, object, ol, output, p, pre, q, ruby, s, samp, section, small, span, strike, strong, sub, summary, sup, table, tbody, td, tfoot, th, thead, time, tr, tt, u, ul, var, video {
