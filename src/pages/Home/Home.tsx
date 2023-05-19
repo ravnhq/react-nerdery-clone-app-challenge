6d462ad0e44b1979b065a88aa3981a6d3c6f9428
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-05-19 10:52:10 -0600

    feat: add base home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { HomeLayout } from '../../Layout/HomeLayout';
+
+const Home = () => (
+    <HomeLayout loading={false}>
+        <>Future home page</>
+    </HomeLayout>
+);
+
+export default Home;
