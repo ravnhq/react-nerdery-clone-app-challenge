78c5d787a18ba8d2345b46d70ba5447dbe035ba6
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-06-07 08:30:15 -0600

    fix: config jest properly

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,4 +6,13 @@ module.exports = {
         '^.+\\.(ts|tsx)$': 'ts-jest',
     },
     testMatch: ['**/*.test.(tsx|jsx)'],
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/**/*.styles.ts', '!src/**/*index.ts', '!src/components/Styles/**/*.{ts,tsx}', 'src/**/*.test.{ts,tsx}'],
+    coverageThreshold: {
+        global: {
+            statements: 50,
+            branches: 50,
+            functions: 50,
+            lines: 50,
+        },
+    },
 }
